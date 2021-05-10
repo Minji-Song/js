9b9484caf22d2dcfbb22e8595a2ae04331547cc8
salswl127 <salswl127@gmail.com>
2021-05-10 18:20:49 +0900

    0510 실습

---
+++ jsLap/WebContent/1장/test1.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+var weights=[50,82,75,120];
+var head2head=	["NLWL","WNLL","LWNW","WWLN"];
+solution(weights, head2head)
+
+
+fuction solution(weights, head2head) {
+	
+	var answer=[];
+	var odds = [];
+	var overs = [];
+	
+	for(var i=0;i<=head2head.length;i++){
+		var odd=0;
+		var over=0;
+		for(var j=0;j<=head2head[i].length;j++){
+			
+		
+		}	
+		
+	}
+	
+	
+}
